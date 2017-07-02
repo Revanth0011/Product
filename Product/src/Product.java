720539ac91247d760f04314d5e55a4c3642464b6
Revanth0011 <revanththanda20@gmail.com>
2017-07-03 00:24:04 +0530

    The code of Product

---
+++ Product/src/Product.java
@@ -0,0 +1,33 @@
+
+public class Product {
+	
+	private int Id;
+	private String Name;
+	private String Price;
+	private String Description;
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public String getPrice() {
+		return Price;
+	}
+	public void setPrice(String price) {
+		Price = price;
+	}
+	public String getDescription() {
+		return Description;
+	}
+	public void setDescription(String description) {
+		Description = description;
+	}
+
+}
